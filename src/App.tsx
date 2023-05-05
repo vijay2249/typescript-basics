d0c34ece7dcfddcf5288da15b52802ed2b7bc4c9
Vijay HCN <vijaychowdary3699@gmail.com>
2023-05-05 09:23:35 +0530

    working with functional props

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import Todos from './components/Todo';
 import Todo from './models/todo'
 import './App.css';
+import NewTodo from './components/NewTodo';
 
 function App() {
   const todos = [
     new Todo('Hello there'),
     new Todo("Bye")
   ]
+
+  const addTodoHandler = (text: string) =>{
+    
+  }
+
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler}/>
       <Todos items={todos}/>
     </div>
   );
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,14 +1,14 @@
 import {useRef} from 'react'
  
-const NewTodo = () =>{
+const NewTodo:React.FC<{onAddTodo: (text: string)=> void}> = (props) =>{
   const todoTextInputRef = useRef<HTMLInputElement>(null)
   const submitHandler = (event: React.FormEvent) =>{
     event.preventDefault()
-    const text = todoTextInputRef.current?.value
-    if(text?.trim().length === 0){
+    const text = todoTextInputRef.current!.value
+    if(text.trim().length === 0){
       return
     }
-    
+    props.onAddTodo(text);
   }
   return (
     <form onSubmit={submitHandler}>
